6bdc7605decfe8bd56b951e0c17e356e970f8d06
Veber <lucasveberdebrida@gmail.com>
2017-03-13 18:37:21 -0300

    comite inicial backend

---
+++ js/connnectDataBase.js
@@ -0,0 +1,90 @@
+
+var pg = require('pg');
+var conString = "postgres://postgres:6587847@localhost/tcc";
+
+
+
+  var query = function (sql, params) {
+    return new Promise(function (resolve, reject) {
+      pg.connect(conString, function (err, client, done) {
+        if (err) {
+        console.log(err);
+        };
+        var query = client.query(sql, params);
+
+        var rows = [];
+
+        query.on('error', function (err) {
+          done();
+          console.log(sql + ' -> ' + params);
+          console.log(err);
+          reject(err);
+        });
+
+        query.on('row', function (row) {
+         rows.push(row);
+        });
+
+        query.on('end', function (result) {
+          done();
+        resolve(rows);
+        });
+      });
+    });
+  };
+
+  module.exports.insert= function(oficina){
+    var query1 = "INSERT INTO oficina(nome, descricao) VALUES($1,$2)";
+    var params = [oficina.nome, oficina.descricao];
+    return query(query1, params)
+  };
+
+
+  module.exports.deleteCheque = function(cheque){
+    var query1 = "DELETE from cheque where nr_cheque = $1";
+    var params = [cheque.nr_cheque];
+    return query(query1, params)
+  };
+
+  module.exports.updateCheque = function(cheque){
+    var query1 = "UPDATE cheque set dt_cheque = $1 , nr_pessoa = $2 where nr_cheque = $3";
+    var params = [cheque.dt_cheque, cheque.nr_pessoa, cheque.nr_cheque];
+    query(query1, params).then(function (result) {
+      console.log("Editado com Sucesso");
+    }).catch(function(error) {
+       console.log(error);
+      });
+  };
+
+  module.exports.consultaCheque = function(cheque){
+    var query1 = "select * from cheque where nr_pessoa = $1";
+    var params = [cheque.nr_pessoa];
+    query(query1, params).then(function (result) {
+      console.log(result);
+    }).catch(function(error) {
+       console.log(error);
+      });
+  };
+
+  module.exports.insertPessoa = function(pessoa){
+    var query1 = "INSERT INTO pessoa(nm_pessoa, pct) VALUES($1,$2)";
+    var params = [pessoa.nmPessoa, pessoa.pct];
+    return query(query1, params)
+  };
+
+
+  module.exports.deletaPessoa = function(pessoa){
+    var query1 = "DELETE from pessoa where nr_pessoa = $1";
+    var params = [pessoa.nr_pessoa];
+    return query(query1, params)
+  };
+
+  module.exports.listAll = function(){
+    var query1 = "select c.dt_cheque, c.nr_cheque, p.nm_pessoa, p.pct, c.vl_cheque, c.vl_receber from cheque c, pessoa p where c.nr_pessoa = p.nr_pessoa";
+    return query(query1, [])
+  };
+
+  module.exports.listCliente = function(){
+    var query1 = "select * from pessoa"
+    return query(query1, [])
+  }
---
+++ js/server.js
@@ -0,0 +1,71 @@
+var express = require("express");
+var app = express();
+var banco = require("./connnectDataBase.js");
+var bodyParser = require('body-parser');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+	//res.send(banco.listAll());
+app.post('/insert', function(req, res){
+	var oficina = req.body;
+	banco.insert(oficina).then(function(resultado){
+		console.log(req);
+		res.send(resultado) ;
+	})
+});
+
+app.get('/consultaCliente', function(req, res){
+	banco.listCliente().then(function(resultado){
+		res.send(resultado) ;
+	})
+});
+
+app.post('/adicionarCheques', function(req, res){
+	var newCheque = req.body;
+	banco.insertCheque(newCheque).then(function(resultado){
+		res.send(resultado) ;
+	})
+});
+
+app.post('/deletaCheque', function(req, res){
+	var delCheque = req.body;
+	banco.deleteCheque(delCheque).then(function(resultado){
+		res.send(resultado) ;
+	})
+});
+
+app.post('/adicionaPessoa', function(req, res){
+	var newPessoa = req.body;
+	banco.insertPessoa(newPessoa).then(function(resultado){
+		res.send(resultado) ;
+	})
+});
+
+app.post('/deletaPessoa', function(req, res){
+	var delPessoa = req.body;
+	banco.deletaPessoa(delPessoa).then(function(resultado){
+		res.send(resultado) ;
+	})
+});
+
+
+var server = app.listen(8081, function(){
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log(host);
+	console.log(port);
+});
+
+
