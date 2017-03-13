22b3be672d5420c3e86b608bb0bb6ae6b9eaf9d6
Veber <lucasveberdebrida@gmail.com>
2017-03-13 19:36:47 -0300

    alterações pavin

--- js/server.js
+++ js/server.js
@@ -11,7 +11,7 @@ app.use(function(req, res, next) {
 });
 
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
@@ -20,7 +20,6 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.post('/insert', function(req, res){
 	var oficina = req.body;
 	banco.insert(oficina).then(function(resultado){
-		console.log(req);
 		res.send(resultado) ;
 	})
 });
