acedbc3fe3a0e89b3fff593f6d656693b0ae15b7
pavinmatheus@gmail.com <pavinmatheus@gmail.com>
2017-05-01 10:56:16 -0300

    Tabelas base

--- scripts/create.sql
+++ scripts/create.sql
@@ -3,6 +3,10 @@ drop schema if exists mymechanic cascade;
 create schema mymechanic;
 
 CREATE SEQUENCE seq_garage START 101;
+CREATE SEQUENCE seq_customer START 101;
+CREATE SEQUENCE seq_user START 101;
+CREATE SEQUENCE seq_review START 101;
+CREATE SEQUENCE seq_answer START 101;
 
 create table mymechanic.garage (
    id integer NOT NULL DEFAULT nextval('seq_garage'),
@@ -17,7 +21,7 @@ create table mymechanic.garage (
 );
 
 create table mymechanic.customer (
-  id integer not null,
+  id integer NOT NULL DEFAULT nextval('seq_customer'),
   name text not null,
   email text not null,
   cep text not null,
@@ -31,8 +35,33 @@ create table mymechanic.customer (
 );
 
 create table mymechanic.user (
+  id integer NOT NULL DEFAULT nextval('seq_user'),
   id_customer integer,
   id_garage integer,
-  foreign key (id_customer) references mymechanic.customer (id),
-  foreign key (id_garage) references mymechanic.garage (id)
+  ADD CONSTRAINT id_customer_fk foreign key (id_customer) references mymechanic.customer (id),
+  ADD CONSTRAINT id_garage_fk foreign key (id_garage) references mymechanic.garage (id)
+);
+
+create table mymechanic.review (
+
+  id integer NOT NULL DEFAULT nextval('seq_review'),
+  id_customer integer,
+  id_garagae integer,
+  id_review_fk,
+  review text,
+  creation_time timestamp with time zone default now(),
+  ADD CONSTRAINT id_review_fkey foreign key (id_review_fk) references mymechanic.review (id)
+  ADD CONSTRAINT id_customer_fkey foreign key (id_customer) references mymechanic.customer (id),
+  ADD CONSTRAINT id_garage_fkey foreign key (id_garage) references mymechanic.garage (id)
+);
+
+create table mymechanic.answer (
+  id integer NOT NULL DEFAULT nextval('seq_answer'),
+  id_garagae integer,
+  id_review,
+  answer text,
+  creation_time timestamp with time zone default now(),
+  ADD CONSTRAINT id_review_fk foreign key (id_review) references mymechanic.review (id)
+  ADD CONSTRAINT id_customer_fk foreign key (id_customer) references mymechanic.customer (id),
+  ADD CONSTRAINT id_garage_fk foreign key (id_garage) references mymechanic.garage (id)
 );
