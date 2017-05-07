2921497601c5cf05f142a997eb4b95ccb47b5b91
mfx <mateusmfx@gmail.com>
2017-05-06 21:34:04 -0300

    birl

--- js/route/sessionRoute.js
+++ js/route/sessionRoute.js
@@ -1,11 +1,8 @@
 const jwt = require('jsonwebtoken');
-const token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 const router = require('express').Router();
 const co = require('co');
 
 router.post('/signin', function (req, res, next) {
-    console.log(req);
-    console.log(req.body.user, req.body.password);
     if (req.body.user === "lucas" && req.body.password === 123456) {
         const token = jwt.sign(req.body, 'cremefraiche');
         res.json(token);
--- js/server.js
+++ js/server.js
@@ -10,7 +10,6 @@ app.all('*', function(req, res, next) {
 	next();
 });
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
